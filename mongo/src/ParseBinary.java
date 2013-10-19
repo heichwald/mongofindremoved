936b88f59974b08a5898a65baddcba8a7220b171
herve eichwald <herve.eichwald@gmail.com>
2013-10-19 17:53:50 -0400

    Added comments

--- mongo/src/ParseBinary.java
+++ mongo/src/ParseBinary.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -51,22 +50,14 @@ public class ParseBinary {
 		return true;
 	}
 
-	static boolean matchWord(List<Byte> in, byte[] token) {
-		return in.size() >= token.length
-				&& equalsBytes(in.subList(in.size() - token.length, in.size()),
-						token);
-	}
-
-	static int int32FromBytes(byte a1, byte a2, byte a3, byte a4) {
-		byte[] intAs4Bytes = new byte[] { a1, a2, a3, a4 };
-		return ByteBuffer.wrap(intAs4Bytes).getInt();
-	}
-
 	public static void main(String[] args) throws IOException {
 		PrintWriter writer = null;
 		try {
 			Path inPath = Paths.get("/home/herve/Downloads/assets.0");
 			writer = new PrintWriter("out");
+			/*
+			 * Look for the end of an image document Storage id is used for that
+			 */
 			LinkedList<Byte> storageIdDel = new LinkedList<>();
 			storageIdDel.add(Byte.valueOf((byte) 105));
 			storageIdDel.add(Byte.valueOf((byte) 100));
@@ -74,17 +65,16 @@ public class ParseBinary {
 			storageIdDel.add(Byte.valueOf((byte) 41));
 			byte[] data = Files.readAllBytes(inPath);
 			LinkedList<Byte> last4bytes = new LinkedList<>();
-			List<Byte> end = new ArrayList<>(4);
-			end.add((byte) 0);
-			end.add((byte) 0);
-			end.add((byte) 0);
-			end.add((byte) 0);
+
+			/*
+			 * Start is _id (07 5F 69 64)
+			 */
 			List<Byte> start = new ArrayList<>();
 			start.add((byte) 7);
 			start.add((byte) 95);
 			start.add((byte) 105);
 			start.add((byte) 100);
-			List<BSONObject> res = new ArrayList<>();
+
 			List<Byte> newImageBytes = null;
 			int endIndex = -1;
 			int count = 0;
@@ -94,30 +84,40 @@ public class ParseBinary {
 				}
 				last4bytes.add(data[i]);
 				if (newImageBytes != null) {
+					// we are building an image doc
 					newImageBytes.add(data[i]);
 				}
 				if (last4bytes.equals(start)) {
-					// System.out.println("yes");
+					boolean ok = true;
 					for (int j = 0; j < 4; j++) {
 						// -18 == 0xEE
-						if (data[i + j - 7] == (byte) -18) {
-							newImageBytes = new ArrayList<>(last4bytes);
+						// previous bytes have to be EE EE EE EE
+						if (data[i + j - 7] != (byte) -18) {
+							ok = false;
 						}
 					}
-				} else if (newImageBytes != null
-						&& last4bytes.equals(storageIdDel)) {
+					if (ok) {
+						// We have found a new image
+						newImageBytes = new ArrayList<>(last4bytes);
+					}
+				} else if (newImageBytes != null && last4bytes.equals(storageIdDel)) {
+					//The end will be after the storage id value
 					endIndex = i + 44;
 				} else if (i == endIndex) {
 					int prepnb = 4;
 					int appnb = 2;
-
+					//Prefix of 4 bytes to store the size of the doc
 					for (int j = 0; j < prepnb; j++) {
 						newImageBytes.add(0, (byte) 0);
 					}
+					//Suffix of two bytes of 0 to close correctly the doc
 					for (int j = 0; j < appnb; j++) {
 						newImageBytes.add((byte) 0);
 					}
 					int finalLength = newImageBytes.size();
+					//First 4 bytes are for the size of the doc
+					//If size in bytes is 200, the four bytes will be 200 0 0 0
+					//If size is > 256, ex: 400 the four bytes will be 144 1 0 0 (144+256 *1)
 					int a = finalLength / 256;
 					int b = finalLength - a * 256;
 
@@ -126,8 +126,7 @@ public class ParseBinary {
 					}
 					newImageBytes.set(0, (byte) b);
 					try {
-						BSONObject bsonRes = new BasicBSONDecoder()
-								.readObject(fromBytetobyte(newImageBytes));
+						BSONObject bsonRes = new BasicBSONDecoder().readObject(fromBytetobyte(newImageBytes));
 						count++;
 						System.out.println(bsonRes);
 						writer.println(bsonRes);
@@ -147,3 +146,4 @@ public class ParseBinary {
 		}
 	}
 }
+
